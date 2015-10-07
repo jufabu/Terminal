38fd630ba7d803d6c50570ee35f054d2e591e09f
jufabu <jufabu@hotmail.com>
2015-10-06 22:49:33 -0200

    alta ticket

--- Terminal/src/com/uy/antel/Socket/ctrlSocket.java
+++ Terminal/src/com/uy/antel/Socket/ctrlSocket.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.math.BigInteger;
 import java.net.Socket;
@@ -176,7 +177,7 @@ public class ctrlSocket {
 					Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 	//				jaxbUnmarshaller.setSchema(schema);
 					//Hago la conversion de XML -> objeto AltaTicket.
-					ticket = (XmlDataTicket) jaxbUnmarshaller.unmarshal(in);
+					ticket = (XmlDataTicket) jaxbUnmarshaller.unmarshal(new StringReader(is.readUTF()));
 					
 					//			} catch (SAXException e) {
 	//				// TODO Auto-generated catch block
@@ -184,6 +185,9 @@ public class ctrlSocket {
 			
 			}
 		}catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
