81541962b2249cd889bc66eb1e3d29a54196def3
miltonrodriguez <miltonrodriguez1985@gmail.com>
2015-10-14 09:10:17 -0300

    subo algo de menu en la interfaz

--- Terminal/src/com/uy/antel/Interfaz.java
+++ Terminal/src/com/uy/antel/Interfaz.java
@@ -46,31 +46,53 @@ public class Interfaz {
 			}
 		}
 		while (true) {
-			/* PIDO QUE INGRESE LOS DATOS */
-			System.out.println("Ingrese los datos en el siguente orden");
-			System.out.println("Matricula fechaInicio(yyyy-MM-dd_HH:mm) duracion(min)");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("------------------------Menu terminal------------------------");
+			System.out.println("1- Alta Ticket");
+			System.out.println("2- Cancelacion Ticket");
 			Scanner entrada = new Scanner(System.in);
-
 			cadena = entrada.nextLine();
-			matricula = cadena.substring(0, 7);
-			fechaIn = cadena.substring(8, 24);
-			duracion = Integer.parseInt(cadena.substring(25, cadena.length()));
-			socket.XmlEnvioAltaTicket(matricula, fechaIn, duracion);
+			try {
+				if (1 == Integer.parseInt(cadena)) {
+					// Alta de ticket
+					/* PIDO QUE INGRESE LOS DATOS */
+					System.out.println("Ingrese los datos en el siguente orden");
+					System.out.println("Matricula fechaInicio(yyyy-MM-dd_HH:mm) duracion(min)");
+					// Scanner entrada = new Scanner(System.in);
 
-			ticket = socket.recibeDataTicket();
-			error = ticket.getXmlRespAltaTicket().getError();
+					cadena = entrada.nextLine();
+					matricula = cadena.substring(0, 7);
+					fechaIn = cadena.substring(8, 24);
+					duracion = Integer.parseInt(cadena.substring(25, cadena.length()));
+					socket.XmlEnvioAltaTicket(matricula, fechaIn, duracion);
 
-			if (error == 0 && ticket.getOperacion().toString() == OperacionT.ALTA.toString()) {
-				mensaje = ticket.getXmlRespAltaTicket().getMensaje();
-				System.out.println(mensaje);
-				System.out.println("Importe a pagar: " + ticket.getXmlRespAltaTicket().getImporteTotal());
-				System.out.println("NroTicket: " + ticket.getXmlRespAltaTicket().getNroTicket());
-				
-				
-			} else {
+					ticket = socket.recibeDataTicket();
+					error = ticket.getXmlRespAltaTicket().getError();
 
-				System.out.println("Error en la terminal");
+					if (error == 0 && ticket.getOperacion().toString() == OperacionT.ALTA.toString()) {
+						mensaje = ticket.getXmlRespAltaTicket().getMensaje();
+						System.out.println(mensaje);
+						System.out.println("Importe a pagar: " + ticket.getXmlRespAltaTicket().getImporteTotal());
+						System.out.println("NroTicket: " + ticket.getXmlRespAltaTicket().getNroTicket());
+
+					} else {
+
+						System.out.println("Error: "+ticket.getXmlRespAltaTicket().getError());
+						System.out.println(ticket.getXmlRespAltaTicket().getMensaje());
+					}
+				} else if (2 == Integer.parseInt(cadena)) {
+					// Cancelacion de ticket
+					System.out.println("Falta Implementar");
+				} else {
+					System.out.println("Opcion Incorrecta");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Se debe ingresar un numero correcto");
 			}
+
 		}
 	}
 
